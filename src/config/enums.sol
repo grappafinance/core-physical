7090f75ac394059c7d24e82dddfa94101402cd06
Vitaly Galaichuk <swissarmytowel@gmail.com>
2023-06-26 20:54:25 +0700

    fix: put ExerciseToken to the end of the enum; change solmate and openzeppelin urls

--- src/config/enums.sol
+++ src/config/enums.sol
@@ -16,12 +16,12 @@ enum ActionType {
     BurnShort,
     AddLong,
     RemoveLong,
-    ExerciseToken,
     SettleAccount,
     // actions that influence more than one subAccounts:
     // These actions are defined in "OptionTransferable"
     MintShortIntoAccount, // increase short (debt) position in one subAccount, increase long token directly to another subAccount
     TransferCollateral, // transfer collateral directly to another subAccount
     TransferLong, // transfer long directly to another subAccount
-    TransferShort // transfer short directly to another subAccount
+    TransferShort, // transfer short directly to another subAccount
+    ExerciseToken
 }
